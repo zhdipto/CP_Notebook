f9d032d7c5c3f7406e4437d4a2d0c3ff4e654023
zhdipto <textzhdipto@gmail.com>
2026-07-20 20:23:44 +0600

    initial update

--- cpNotebook/accounts/serializers.py
+++ cpNotebook/accounts/serializers.py
@@ -9,7 +9,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password']
 
     def validate_password(self, value):
         validate_password(value)
@@ -17,7 +17,10 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User(
-            username=validated_data['username'], email=validated_data.get('email', '')
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
         )
         user.set_password(validated_data['password'])
         user.save()
